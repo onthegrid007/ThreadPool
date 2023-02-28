529c930e1dfdd81064cbaf6d510022cce2c1ce44
onthegrid007 <peter.ferranti.dev@gmail.com>
2023-02-28 13:58:03 -0500

    .

--- threadpool.cpp
+++ threadpool.cpp
@@ -9,57 +9,99 @@ _SCM_CHILD_DEFINITIONS(ThreadPool)
 
 const ThreadPool::Task ThreadPool::NullTask = std::move([](){});
 
-void ThreadPool::WorkerRun(ThreadPool* pool, Worker* self) {
-	self->ID = std::this_thread::get_id();
-	pool->m_workerSem->waitFor([&](const int64_t cVal, const int64_t CInitVal){ return (self->Thread != nullptr); });
-	pool->m_onTBegin();
-	Task T;
-	pool->m_workerSem->waitFor([&](const int64_t cVal, const int64_t CInitVal){ return (pool->m_poolState == PoolState::RUNNING); });
-	while(pool->m_poolState == PoolState::RUNNING) {
-		pool->m_taskSem->waitFor([&](const int64_t cVal, const int64_t CInitVal){ return (!pool->m_tasks.empty()) || (pool->m_poolState != PoolState::RUNNING); });
-		if(pool->m_poolState == PoolState::SHUTTING_DOWN) break;
-		auto* lock = new std::lock_guard<std::mutex>(*pool->m_taskMTX);
-		if(!pool->m_tasks.empty()) {
-			T = pool->m_tasks.front();
-			pool->m_tasks.pop_front();
-			pool->m_taskSem->notify();
-			delete lock;
-			try {
-				T();
-			} catch(std::exception& e) {
-				// e.what();
-			}
+void ThreadPool::SIGHandler(int sig, siginfo_t* info, void* extra) {
+	// BADLOGV(sig);
+	ThreadPool* pool = (info->si_ptr ? (ThreadPool*)info->si_ptr : nullptr);
+	// BADLOG("caught: " << int(sig));
+	switch(sig) {
+		case SIGUSR1:
+		if(pool) {
+			// const __pid_t selfID = gettid();
+			// for(auto worker = pool->m_workers.begin(); worker != pool->m_workers.end(); worker++) {
+			// 	if(worker->ID == selfID) {
+					throw "S";
+				// }
+			// }
 		}
-		else {
-			delete lock;
-			T = ThreadPool::NullTask;
+		break;
+		case SIGUSR2:
+		if(pool) {
+			while(pool->m_poolState.load() == PoolState::PAUSED) pause();
+		}
+		break;
+		default:
+		break;
+	};
+}
+
+void ThreadPool::PauseToggle() {
+	if(m_poolState == PoolState::RUNNING) {
+		m_poolState.store(PoolState::PAUSED);
+		m_pauseMTX.lock();
+		for(auto& worker : m_workers) {
+			sigqueue(worker.ID, SIGUSR2, m_sv);
 		}
 	}
-	pool->m_onTEnd();
-	// {
-	// 	std::lock_guard<std::mutex> lock(*pool->m_workerMTX);
-	// 	// self->Thread->detach();
-	// 	self->Thread = nullptr;
-	// 	self->ID = {};
-	// 	for(auto itt = begin(pool->m_workers); itt != end(pool->m_workers); itt++) {
-	// 		if(&(*itt) == self) { pool->m_workers.erase(itt); break; }
-	// 	}
-	// }
-		pool->m_workerSem->dec();
-		pool->m_workerSem->notify();
+	else if(m_poolState == PoolState::PAUSED) {
+		m_poolState.store(PoolState::RUNNING);
+		for(auto& worker : m_workers) {
+			sigqueue(worker.ID, SIGPOLL, m_sv);
+		}
+		sleep_for(seconds(1));
+		m_pauseMTX.unlock();
+		if(m_missedTaskEnqueues) {
+			while(m_missedTaskEnqueues) {
+				m_taskSem.notify_one();
+				m_missedTaskEnqueues--;
+			}
+		}
+	}
 }
 
 void ThreadPool::AllocateWorker() {
-	std::lock_guard<std::mutex> lock(*m_workerMTX);
-	if(m_workers.size() < m_maxWorkerCount) {
-		m_workers.emplace_back(Worker{{}, nullptr});
-		ThreadPool::Worker& newWorker = m_workers.back();
-		newWorker.Thread = std::move(new std::thread(ThreadPool::WorkerRun, this, &newWorker));
+	if((m_workers.size() < m_maxWorkerCount) && (m_poolState == PoolState::RUNNING)) {
+		std::lock_guard<std::mutex> lock(m_workerMTX);
+		if(m_workers.size() >= m_maxWorkerCount) return;
+		m_workers.emplace_back();
+		m_workers.back().Thread = std::make_unique<std::thread>([=](Worker* self){
+			sigjmp_buf RunTask, SigChk;
+			self->ID = gettid();
+			// BADLOGV(int64_t(self->ID));
+			if(sigaction(SIGUSR1, &m_siga, NULL) != 0) throw std::runtime_error("Unable to set SIGUSR1 handler on new worker!");
+			if(sigaction(SIGUSR2, &m_siga, NULL) != 0) throw std::runtime_error("Unable to set SIGUSR2 handler on new worker!");
+			if(sigaction(SIGPOLL, &m_siga, NULL) != 0) throw std::runtime_error("Unable to set SIGPOLL handler on new worker!");
+			m_onTBegin();
+			Task T;
+			while((m_poolState.load() != PoolState::SHUTTING_DOWN) && (m_poolState.load() != PoolState::INACTIVE)) {
+				if(sigsetjmp(RunTask, 1)) {
+					try {
+						T();
+					} catch(const char* e) {
+						// std::string(e.what()) == "S" ? longjmp(SigChk, 0) : void();
+					}
+				}
+				sigsetjmp(SigChk, 1);
+				BADLOGV(int(m_poolState.load()));
+				try {
+					m_taskSem.waitFor([=](const int64_t cVal, const int64_t cInitVal){ return (!m_tasks.empty()) || (m_poolState.load() == PoolState::SHUTTING_DOWN) || (m_poolState.load() == PoolState::INACTIVE); });
+					if((m_poolState == PoolState::SHUTTING_DOWN) || (m_poolState == PoolState::INACTIVE)) break;
+					std::lock_guard<std::mutex> lock(m_taskMTX);
+					if(!m_tasks.empty()) {
+						T = std::move(m_tasks.front());
+						m_tasks.pop_front();
+						throw "T";
+					}
+				} catch(const char* e) {
+					std::string(e) == "T" ? siglongjmp(RunTask, 1) : void();
+					std::string(e) == "S" ? siglongjmp(SigChk, 0) : void();
+				}
+			}
+		}, &m_workers.back());
 	}
 }
 
 ThreadPool::ThreadPool(int64_t maxWorkerCount, bool isOverflowBlocking, int64_t maxQueueSize, Task onTBegin, Task onTEnd) :
-	m_poolState(PoolState::INACTIVE),
+	m_poolState(PoolState::RUNNING),
 	m_maxWorkerCount((maxWorkerCount < 2) ? 2 : maxWorkerCount),
 	m_isOverflowBlocking(isOverflowBlocking),
 	m_maxQueueSize(maxQueueSize),
@@ -67,22 +109,30 @@ ThreadPool::ThreadPool(int64_t maxWorkerCount, bool isOverflowBlocking, int64_t 
 	m_workers(),
 	m_onTBegin(onTBegin),
 	m_onTEnd(onTEnd),
-	m_workerSem(new Semaphore),
-	m_taskSem(new Semaphore),
-	m_workerMTX(new std::mutex),
-	m_taskMTX(new std::mutex) {
+	m_workerSem(0),
+	m_taskSem(0),
+	m_workerMTX(),
+	m_taskMTX(),
+	m_sv({.sival_ptr = (void*)this}) {
 	for(char i = 0; i < 2; i++)
 		AllocateWorker();
-	m_poolState = PoolState::RUNNING;
-	m_workerSem->notify();
 }
 
-	ThreadPool::~ThreadPool() {
-		m_poolState = PoolState::SHUTTING_DOWN;
-		while(int64_t(*m_workerSem) > 0) {
-			m_workerSem->notify();
-			m_taskSem->notify();
+ThreadPool::~ThreadPool() {
+	m_poolState.store(PoolState::SHUTTING_DOWN);
+	if(m_poolState == PoolState::PAUSED) {
+		m_paused.store(false);
+		for(auto& worker : m_workers) {
+			sigqueue(worker.ID, SIGPOLL, m_sv);
 		}
-		delete m_taskSem;
-		delete m_workerSem;
+		sleep_for(seconds(1));
 	}
+	for(auto& worker : m_workers) {
+		while(!(worker.Thread.get()->joinable())) {
+			m_taskSem.notify();
+			sigqueue(worker.ID, SIGUSR1, m_sv);
+		}
+		m_taskSem.notify();
+		worker.Thread.get()->join();
+	}
+}
--- threadpool.h
+++ threadpool.h
@@ -6,17 +6,23 @@
 
 #ifndef THREADPOOL_H_
 #define THREADPOOL_H_
-
+#include <signal.h>
+#include <setjmp.h>
 #include <thread>
 #include <deque>
 #include <vector>
+#include <functional>
+#include <future>
+#include <utility>
+#include <stdexcept>
+#include <type_traits>
+
+#include "vendor/Semaphore/vendor/Singleton/singleton_container_map.hpp"
+#include "vendor/ADVClock/vendor/Timestamp/timestamp.hpp"
+#include "vendor/Semaphore/semaphore.h"
 
 #define BADLOG(x) std::cout << x << std::endl;
 #define BADLOGV(x) BADLOG(#x << ":") BADLOG(x << std::endl)
-
-#include "vendor/Semaphore/vendor/Singleton/singleton_container_map.hpp"
-#include "vendor/Timestamp/timestamp.hpp"
-#include "vendor/Semaphore/semaphore.h"
 
 using namespace std::this_thread;
 using namespace std::chrono;
@@ -25,59 +31,89 @@ class ThreadPool : public SingletonContainerMap<ThreadPool> {
 	public:
 	typedef std::function<void(void)> Task;
 	typedef std::thread::id TIDType;
+	typedef __pid_t PIDType;
 	enum PoolState : char {
 		RUNNING = 0,
-		SHUTTING_DOWN = 1,
+		PAUSED = 1,
+		SHUTTING_DOWN = 2,
 		INACTIVE = -1
 	};
+	void PauseToggle();
 	
 	private:
 	_SCM_CHILD_DECLORATIONS(ThreadPool)
+	static void SIGHandler(int sig, siginfo_t* info, void* extra);
 	typedef struct Worker_t {
-		TIDType ID;
-		std::thread* Thread = nullptr;
+		std::unique_ptr<std::thread> Thread;
+		PIDType ID;
 		Timestamp Begin;
 	} Worker;
 	static const Task NullTask;
 	Task m_onTBegin = NullTask;
 	Task m_onTEnd = NullTask;
-	static void WorkerRun(ThreadPool* pool, ThreadPool::Worker* self);
 	void AllocateWorker();
 	int64_t m_maxWorkerCount = 2;
 	int64_t m_maxQueueSize = 32;
 	std::deque<Task> m_tasks = {};
-	std::vector<Worker> m_workers = {};
+	std::vector<Worker> m_workers;
 	Timestamp m_begin;
-	Semaphore* m_workerSem = nullptr;
-	Semaphore* m_taskSem = nullptr;
-	std::mutex* m_workerMTX = nullptr;
-	std::mutex* m_taskMTX = nullptr;
-	PoolState m_poolState = PoolState::INACTIVE;
+	Semaphore m_workerSem;
+	Semaphore m_taskSem;
+	std::mutex m_workerMTX;
+	std::mutex m_taskMTX;
+	std::mutex m_pauseMTX;
+	std::atomic<bool> m_paused; // = PoolState::INACTIVE;
+	std::atomic<PoolState> m_poolState; // = PoolState::INACTIVE;
+	static constexpr struct sigaction m_siga = {.sa_sigaction = ThreadPool::SIGHandler, .sa_mask = {}, .sa_flags = SA_SIGINFO | SA_NODEFER, .sa_restorer = nullptr};
+	const __sigval_t m_sv;
 	bool m_isOverflowBlocking = true;
+	std::atomic<int> m_missedTaskEnqueues; // = 0;
+	
 	ThreadPool(int64_t maxWorkerCount = 2, bool isOverflowBlocking = true, int64_t maxQueueSize = 32, Task onTBegin = NullTask, Task onTEnd = NullTask);
 	~ThreadPool();
 	
 	public:
-	template<typename Func, typename... Args>
-	ThreadPool& enqueue_work(Func&& F, Args&& ... args) {
+	template<typename F, typename... Args, typename RTN_T = std::invoke_result_t<std::decay_t<F>, std::decay_t<Args>...>>
+	auto enqueue_work(F f, Args... args) -> std::future<RTN_T> {
+		auto tprom = std::make_shared<std::promise<RTN_T>>();
 		if(m_poolState != PoolState::INACTIVE) {
+			jmp_buf trySemWait;
 			if(m_workers.size() < m_maxWorkerCount) AllocateWorker();
-			try_enqueue:
-			if((m_tasks.size() >= m_maxQueueSize) && m_isOverflowBlocking) m_taskSem->waitFor([&](const int64_t cVal, const int64_t CInitVal){ return (m_tasks.size() < m_maxQueueSize); });
-			
-		auto* lock = new std::lock_guard<std::mutex>(*m_taskMTX);
-				if(m_tasks.size() < m_maxQueueSize) {
-					m_tasks.emplace_back(std::move([&](){ F(args...); }));
-					delete lock;
-					m_taskSem->notify();
-				}
-				else {
-					delete lock;
-					goto try_enqueue;
-				}
+			std::function<RTN_T(Args...)> tfunc = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+			// try_enqueue:
+			setjmp(trySemWait);
+			if((m_tasks.size() >= m_maxQueueSize) && m_isOverflowBlocking) {
+				std::lock_guard<std::mutex> lock(m_pauseMTX);
+				m_taskSem.waitFor([&](const int64_t cVal, const int64_t CInitVal){ return (m_tasks.size() < m_maxQueueSize); });
 			}
-		// }
-		return *this;
+			
+			m_taskMTX.lock();
+			if(m_tasks.size() < m_maxQueueSize) {
+				m_tasks.emplace_back([tfunc, tprom](){
+					try {
+						if constexpr(std::is_void_v<RTN_T>) {
+							std::invoke(tfunc);
+							tprom->set_value();
+						}
+						else {
+							tprom->set_value(std::invoke(tfunc));
+						}
+					}
+					catch (...) {
+						try {
+							tprom->set_exception(std::current_exception());
+						} catch (...) {}
+					}
+            	});
+				m_taskMTX.unlock();
+				if(m_paused.load()) { m_missedTaskEnqueues++; } else { m_taskSem.notify_one(); }
+			}
+			else {
+				m_taskMTX.unlock();
+				longjmp(trySemWait, 0); // try_enqueue;
+			}
+		}
+		return tprom->get_future();
 	}
 };
 #endif
